c91f6d9e48471790a2547df8e1bf468cbb39bb31
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-12-02 18:10:37 +0300

    feat: add event pagination

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import EventItem from "./EventItem";
-import { Stack, Typography } from "@mui/material";
+import { Container, Grid, Pagination, Stack, Typography } from "@mui/material";
+import { ITEMS_PER_PAGE } from "../utils/consts";
 
 const EventList = ({ events, deleteHandler }) => {
+    const [page, setPage] = useState(1);
+    const [pageEvents, setPageEvents] = useState([]);
+
+    const pagesCount = useMemo(() => {
+        var count = Math.floor(events.length / ITEMS_PER_PAGE);
+
+        if (events.length % ITEMS_PER_PAGE !== 0) {
+            count++;
+        }
+
+        return count;
+    }, [events]);
+
+    useEffect(() => {
+        setPageEvents(events.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE));
+    }, [events, page])
+
+    const changePage = (event, value) => {
+        setPage(value);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     if (events.length === 0) {
-        return <Typography variant="h4" mt={2}>Мероприятия не найдены</Typography>;
+        return (
+            <Typography variant="h4" mt={2}>
+                Мероприятия не найдены
+            </Typography>
+        );
     }
 
     return (
         <Stack gap={1}>
-            {events.map((event) => (
+            {pageEvents.map((event) => (
                 <EventItem key={event.id} event={event} deleteHandler={deleteHandler} />
             ))}
+            <Container>
+                <Grid container justifyContent="center">
+                    <Pagination
+                        count={pagesCount}
+                        color="primary"
+                        shape="rounded"
+                        page={page}
+                        onChange={changePage}
+                    ></Pagination>
+                </Grid>
+            </Container>
         </Stack>
     );
 };
--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -3,3 +3,5 @@ export const MAINPAGE_ROUTE = "/events";
 export const EVENTDETAILS_ROUTE = "/events/:id";
 export const OFFENSIVE_ROUTE = "/offensive";
 export const REPORTS_ROUTE = "/reports";
+
+export const ITEMS_PER_PAGE = 10;
