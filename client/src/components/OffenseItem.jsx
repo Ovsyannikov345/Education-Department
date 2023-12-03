50ced9810730b24275c231c8defbc0bc1c4ed8e6
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-12-03 21:36:15 +0300

    feat: style the offense item in list

--- client/src/components/OffenseItem.jsx
+++ client/src/components/OffenseItem.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Grid, Typography } from "@mui/material";
 import moment from "moment";
 import DeleteConfirmationModal from "../modal/DeleteConfirmationModal/DeleteConfirmationModal";
+import EvenIcon from "@mui/icons-material/Event";
 
 const OffenseItem = ({ offense, deleteHandler }) => {
     const [deleteModalOpen, setDeleteModalOpen] = useState(false);
@@ -25,18 +26,72 @@ const OffenseItem = ({ offense, deleteHandler }) => {
                 borderRadius={2}
                 sx={{ border: 1, borderColor: "#1976d29F" }}
             >
-                <Grid item xs={12}>
-                    <Typography variant="h5" overflow={"hidden"} whiteSpace={"nowrap"} textOverflow={"ellipsis"}>
-                        {`${offense.Student.lastName} ${offense.Student.firstName} ${offense.Student.patronymic}`}
-                    </Typography>
-                    <Typography variant="h6">{offense.Student.groupName}</Typography>
-                    <Typography variant="h6">{moment.utc(offense.offenseDate).format("DD-MM-YYYY")}</Typography>
-                    <Typography variant="h6">{offense.article}</Typography>
-                    <Typography variant="h6">{offense.courtDecision}</Typography>
-                    <Typography variant="h6">{offense.penalty}</Typography>
-                    <Button variant="outlined" color="error" onClick={() => setDeleteModalOpen(true)}>
-                        Удалить
-                    </Button>
+                <Grid container item xs={12}>
+                    <Grid container item xs={12} gap={7}>
+                        <Grid item>
+                            <Typography
+                                variant="h5"
+                                overflow={"hidden"}
+                                whiteSpace={"nowrap"}
+                                textOverflow={"ellipsis"}
+                            >
+                                {`${offense.Student.lastName} ${offense.Student.firstName} ${offense.Student.patronymic}`}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h6">{offense.Student.groupName}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={12} alignItems={"center"} gap={0.5}>
+                        <EvenIcon />
+                        <Typography variant="h6">
+                            {moment.utc(offense.offenseDate).format("DD-MM-YYYY")}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="subtitle1" style={{ textDecoration: "underline" }}>
+                            <b>Статья</b>
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            overflow={"hidden"}
+                            whiteSpace={"nowrap"}
+                            textOverflow={"ellipsis"}
+                        >
+                            {offense.article}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Typography variant="subtitle1" style={{ textDecoration: "underline" }}>
+                            <b>Решение суда</b>
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            overflow={"hidden"}
+                            whiteSpace={"nowrap"}
+                            textOverflow={"ellipsis"}
+                        >
+                            {offense.courtDecision}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1" style={{ textDecoration: "underline" }}>
+                            <b>Штраф</b>
+                        </Typography>
+                        <Typography variant="h6">{offense.penalty}</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container item xs={12} gap={2}>
+                    <Grid item>
+                        <Button variant="outlined" color="primary">
+                            Подробнее
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="outlined" color="error" onClick={() => setDeleteModalOpen(true)}>
+                            Удалить
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
         </>
