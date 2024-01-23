44dc9f91f4e2ec01203d157e138c5ca6cda775bc
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-23 22:58:30 +0300

    feat: add auth host

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,12 @@
 import axios from "axios";
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.common["Authorization"] = localStorage.getItem("accessToken");
 
 const host = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
 });
 
-export { host };
+const authHost = axios.create({
+    baseURL: process.env.REACT_APP_AUTH_URL,
+});
+
+export { host, authHost };
