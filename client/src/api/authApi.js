e47a4b2831fa48608c78d9159ea52e104ec70fc3
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-23 22:58:51 +0300

    feat: add auth api

---
+++ client/src/api/authApi.js
@@ -0,0 +1,20 @@
+import { authHost } from ".";
+
+const login = async ({ email, password }) => {
+    try {
+        console.log(process.env.API_URL);
+        const response = await authHost.post("/login", { email: email, password: password });
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            return error.response;
+        } else if (error.request) {
+            console.log("Server did not respond.");
+        } else {
+            console.log("Error while creating request");
+        }
+    }
+};
+
+export { login };
