8a2dc4118bac22f5743feb7acb9512b691fc48f0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-25 23:28:17 +0300

    feat: add reload after login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -46,6 +46,7 @@ const LoginPage = () => {
                     if (response.status < 400) {
                         localStorage.setItem("accessToken", response.data.accessToken);
                         localStorage.setItem("refreshToken", response.data.refreshToken);
+                        window.location.reload();
                     } else {
                         displayError(response.data.error);
                     }
