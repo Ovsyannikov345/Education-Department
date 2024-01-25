a3f29f2dcb0f4800df4bdc4c4efdf091693fd011
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-25 23:28:33 +0300

    feat: remove local storage tracking

--- client/src/components/Router/AppRouter.js
+++ client/src/components/Router/AppRouter.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { publicRoutes } from "./publicRoutes";
 import { userRoutes } from "./userRoutes";
 import { LOGIN_ROUTE, MAINPAGE_ROUTE } from "../../utils/consts";
 
 const AppRouter = () => {
-    const [accessToken, setAccessToken] = useState(localStorage.getItem("accessToken"));
-
-    useEffect(() => {
-        const iframe = document.createElement("iframe");
-        iframe.style.display = "none";
-
-        document.body.appendChild(iframe);
-        iframe.contentWindow.addEventListener("storage", () => {
-            setAccessToken(localStorage.getItem("accessToken"));
-        });
-
-        return () => {
-            iframe.contentWindow.removeEventListener("storage", () => {
-                setAccessToken(localStorage.getItem("accessToken"));
-            });
-            document.body.removeChild(iframe);
-        };
-    }, []);
+    const accessToken = localStorage.getItem("accessToken");
 
     if (!accessToken) {
         return (
