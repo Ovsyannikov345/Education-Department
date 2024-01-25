3afe56d7385dd0167ad7f9666b8d4531773f5358
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-25 23:23:06 +0300

    feat: add token update function

---
+++ client/src/utils/updateToken.js
@@ -0,0 +1,26 @@
+import { host, authHost } from "../api";
+
+const updateToken = async (requestFunction, argument) => {
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    try {
+        const response = await authHost.post("/refresh", { token: refreshToken });
+
+        const accessToken = response.data.accessToken;
+
+        localStorage.setItem("accessToken", accessToken);
+        host.defaults.headers.common["Authorization"] = localStorage.getItem("accessToken");
+
+        if (argument === undefined) {
+            return await requestFunction();
+        }
+
+        return await requestFunction(argument);
+    } catch (error) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.reload();
+    }
+};
+
+export default updateToken;
