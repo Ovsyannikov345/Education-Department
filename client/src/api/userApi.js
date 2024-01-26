37932aad963170d91775e9dd6119e1e800f38e05
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-27 02:04:58 +0300

    feat: implement user api

---
+++ client/src/api/userApi.js
@@ -0,0 +1,83 @@
+import { host } from ".";
+import updateToken from "./../utils/updateToken";
+
+const getUsers = async () => {
+    try {
+        const response = await host.get("/users");
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            console.log("Error while loading users. Code: " + error.response.status);
+
+            if (error.response.status === 401) {
+                return await updateToken(getUsers);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { error: "Сервис временно недоступен" } };
+        } else {
+            return { data: { error: "Ошибка при создании запроса" } };
+        }
+        // TODO all api calls like this.
+    }
+};
+
+const postUser = async (userData) => {
+    // TODO implement.
+};
+
+const blockUser = async (id) => {
+    try {
+        const response = await host.post(`/users/${id}/block`);
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            console.log("Error while blocking the user. Code: " + error.response.status);
+
+            if (error.response.status === 401) {
+                return await updateToken(blockUser, id);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            console.log("Server did not respond.");
+        } else {
+            console.log("Error while creating request");
+        }
+    }
+};
+
+const unblockUser = async (id) => {
+    try {
+        const response = await host.post(`/users/${id}/unblock`);
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            console.log("Error while unblocking the user. Code: " + error.response.status);
+
+            if (error.response.status === 401) {
+                return await updateToken(unblockUser, id);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            console.log("Server did not respond.");
+        } else {
+            console.log("Error while creating request");
+        }
+    }
+};
+
+const changeUserPassword = async (userData) => {
+    // TODO implement.
+};
+
+const sendNewPassword = async (email) => {
+    // TODO implement.
+};
+
+export { getUsers, postUser, blockUser, unblockUser, changeUserPassword, sendNewPassword };
