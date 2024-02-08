fd140cab5dbfdca47cb06ea6b57cf59b90d25ac8
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-02-08 22:17:42 +0300

    feat: create event validation function

---
+++ client/src/utils/validateFunctions/validateEvent.js
@@ -0,0 +1,44 @@
+const validateEvent = (values, department, direction) => {
+    const errors = {};
+
+    if (!values.departmentId) {
+        errors.departmentId = "Обязательное поле";
+    } else if (department.Subdepartments && department.Subdepartments.length > 0 && !values.subdepartmentId) {
+        errors.subdepartmentId = "Обязательное поле";
+    }
+
+    if (!values.directionId) {
+        errors.directionId = "Обязательное поле";
+    } else if (direction.Subdirections && direction.Subdirections.length > 0 && !values.subdirectionId) {
+        errors.subdirectionId = "Обязательное поле";
+    }
+
+    if (!values.name) {
+        errors.name = "Обязательное поле";
+    } else if (values.name.length > 100) {
+        errors.name = "Длина не более 100 символов";
+    }
+
+    if (values.description.length > 255) {
+        errors.description = "Длина не более 255 символов";
+    }
+
+    if (values.plannedResult.length > 255) {
+        errors.plannedResult = "Длина не более 255 символов";
+    }
+
+    if (!values.date) {
+        errors.date = "Обязательное поле";
+    } else if (values.date === "Invalid date") {
+        errors.date = "Некорректное значение";
+    }
+
+    if (!values.time) {
+        errors.time = "Обязательное поле";
+    }
+    console.log(errors);
+
+    return errors;
+};
+
+export default validateEvent;
