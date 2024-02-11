5a1a05db6a4f0c385beba437be1fbc3602238588
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-02-12 01:04:02 +0300

    feat: move logic to form

--- client/src/pages/AddOffensePage.jsx
+++ client/src/pages/AddOffensePage.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 import CreateOffenseForm from "../components/CreateOffenseForm";
-import { postOffense } from "../api/offensesApi";
 
 const AddOffensePage = () => {
-    const createOffense = async (offense) => {
-        const response = await postOffense(offense);
-
-        if (!response.status || response.status >= 300) {
-            return response.data.error;
-        }
-
-        return "";
-    };
-
-    return <CreateOffenseForm creationHandler={createOffense} />;
+    return <CreateOffenseForm />;
 };
 
 export default AddOffensePage;
