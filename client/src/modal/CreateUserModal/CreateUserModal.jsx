eb75aecd76e5a7c888a4ff539c166f0562559074
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-02-12 01:03:25 +0300

    feat: add fields length validation

--- client/src/modal/CreateUserModal/CreateUserModal.jsx
+++ client/src/modal/CreateUserModal/CreateUserModal.jsx
@@ -23,10 +23,18 @@ const CreateUserModal = ({ open, closeHandler, errorCallback, successCallback })
 
             if (!lastName) {
                 errors.lastName = "Обязательное поле";
+            } else if (lastName.length > 50) {
+                errors.lastName = "Длина не более 50 символов";
             }
 
             if (!firstName) {
                 errors.firstName = "Обязательное поле";
+            } else if (firstName.length > 50) {
+                errors.firstName = "Длина не более 50 символов";
+            }
+
+            if (patronymic && patronymic.length > 50) {
+                errors.patronymic = "Длина не более 50 символов";
             }
 
             return errors;
